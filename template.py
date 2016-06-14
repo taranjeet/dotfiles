b5abc8ebb90fb705391c0f4a78bab68bee33c1e9
Taranjeet <reachtotj@gmail.com>
2016-06-14 19:49:48 +0530

    Add template for c++ program

---
+++ template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+''' 
+	Template for competitive programming
+	C++ 4.9
+'''
+
+
+import sys
+import datetime
+
+PLATFORM = 'codechef'
+
+if len(sys.argv) == 2:
+	name=sys.argv[1]
+else:
+	name='test'
+filename=name+'.cpp'
+f1=open(filename,'w')
+now=datetime.datetime.now()
+time = now.strftime("%Y-%m-%d %H:%M")
+
+string='''
+/*
+Problem : {problem}
+Time    : {time}
+Platform: {platform}
+*/
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+typedef long long int ll;
+
+#define FOR(i,n) for(int i = 0; i < n; i++)
+#define FORS(i,a,n) for(int i = a; i < n; i++)
+#define RDARR(a,n) FOR(i,n) cin>>a[i];
+#define SOLVE() int t;cin>>t;FOR(tc,t) solve();
+#define PB push_back
+
+void solve(){{
+}}
+
+int main(){{ _
+
+	//SOLVE()
+	solve();
+	return 0;
+}}'''.format(time=time, problem=name.upper(), platform=PLATFORM)
+f1.write(string)
+f1.close()
